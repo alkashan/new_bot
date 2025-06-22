d4ecdba01be746b179e2ef2f31af7b11f845855b
Alexandr Yegorov <ae5609836@gmail.com>
2025-06-22 14:53:39 +0300

    навел марафет

--- app/common/users_list.py
+++ app/common/users_list.py
@@ -1,7 +1,11 @@
-ADMINS = [
-    # 1567993777,
-]
+from os import getenv
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv()
+find_dotenv()
+
+ADMINS = list(map(int, getenv('ADMINS').split(',')))
 
 FOREMANS = [
-    # 1567993777,
+    
 ]
